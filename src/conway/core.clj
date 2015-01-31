0496fb7bd78ae6bedb279e344a54625d0914d0aa
Vladymir Bezerra <vlad.bezerra@gmail.com>
2015-01-31 10:50:01 -0300

    Change tabs to spaces

--- src/conway/core.clj
+++ src/conway/core.clj
@@ -1,15 +1,15 @@
 (ns conway.core)
 
 (defn make-grid [line column]
-	(for [c (range line)] (for [r (range column)] 0)))
+    (for [c (range line)] (for [r (range column)] 0)))
 	
 (defn left-of [line column] 
-	(for [x (range (- line 1) (+ line 2))] 
-		[x (- column 1)]))
+    (for [x (range (- line 1) (+ line 2))] 
+        [x (- column 1)]))
 
 (defn right-of [line column height width]
-	(cond
-		(and (> line 0) (< line (- height 1)) (> column 0) (< column (- width 1)))
-			(for [x (range (- line 1) (+ line 2))] 
-				[x (+ column 1)])
-		))
+    (cond
+        (and (> line 0) (< line (- height 1)) (> column 0) (< column (- width 1)))
+            (for [x (range (- line 1) (+ line 2))] 
+                [x (+ column 1)])
+        ))
