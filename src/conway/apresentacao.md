f4806d0c2b3024ed4611d8f30a435c373b07b654
Vladymir Bezerra <vlad.bezerra@gmail.com>
2015-02-13 06:38:52 -0300

    [Conway] More help functions.

---
+++ src/conway/apresentacao.md
@@ -0,0 +1,22 @@
+## Clojure
+
+- Caracteristicas Basicas
+    * Lisp
+      + Criada por John Mcarty
+      + Inspirada no lambda calculus Church 1939
+    * Notação prefixada;
+      Chamada de função em geral: function(param1, param2, ..., paramN);
+      (function param1 param2)
+      Operadores tambem sao funçoes +,-,/,*
+      (+ 10 20 30)   => 60
+      (= true false) => false
+      Numero arbitrário de parametros
+      (* 1 2 3 4 5 6 ...)
+      Expressoes aninhadas
+      (+ 10 (* 3 2) (- 10 3))
+      Ordem de avaliaçao da esquerda pra direita e de dentro pra fora
+      (+ 10 (+ 5 (+ 3 (+ 3 4))))
+    * Parentesis ;)
+- Homoiconicity
+- Estruturas de dados imutáveis (persistentes)
+- Programação Funcional
--- src/conway/core.clj
+++ src/conway/core.clj
@@ -1,22 +1,63 @@
 (ns conway.core)
 
-(defn make-grid [line column]
-    (for [c (range line)] (for [r (range column)] 0)))
-	
-(defn left-of [line column] 
-    (for [x (range (- line 1) (+ line 2))] 
-        [x (- column 1)]))
+(defn randomize []
+    (if (< (rand 100) 25) 1 0))
 
-(defn right-of [line column height width]
-    (cond
-        (and (> line 0) (< line (dec height)) (> column 0) (< column (dec width)))
-            (for [x (range (- line 1) (+ line 2))] 
-                [x (+ column 1)])
-        (= column (dec width)) 
-            (cond (= line 0) [[(dec height) 0] [0 0] [1 0]]
-                  (= line (dec height)) [[(dec (dec height)) 0] [(dec height) 0] [0 0]]
-                  :else
-                    (for [x (range (dec line) (inc (inc line)))] 
-                        [x 0])
-            ) 
-        ))
+(defn make-grid [size]
+    (into [] (for [c (range size)] 
+        (into [] (for [r (range size)] (randomize))))))
+
+
+(defn vertical [line column func]
+    (for [x (range (- line 1) (+ line 2))] 
+        [x (func column 1)]))
+
+(defn horizontal [line column func]
+    (for [x (range (- column 1) (+ column 2))] 
+        [(func line 1) x]))
+
+(defn left-of [line column] 
+    (vertical line column -))
+
+(defn right-of [line column]
+    (vertical line column +))
+
+(defn above-of [line column]
+    (horizontal line column -))
+
+(defn below-of [line column]
+    (horizontal line column +))
+
+(defn neighbours [line column]
+    (into [] 
+        (set (mapcat concat ((juxt left-of 
+                                   right-of 
+                                   above-of 
+                                   below-of) line column)))))
+
+(defn count-neighbours [line column world]
+    (count (filter #(= 1 %) 
+        (map #(get-cell % world) 
+            (normalize (neighbours line column) (count world))))))
+
+(defn make-neighbours-count [size world]
+    (into [] (for [c (range size)] 
+        (into [] (for [r (range size)] (count-neighbours c r world))))))
+
+(defn make-bounds [size]
+    (fn [x] (cond (< x 0)    (- size 1)
+                  (= x size)  0
+                  :else       x)))
+
+(defn get-cell [[line column] matrix]
+    (nth (nth matrix line) column))
+
+(defn normalize [neighbours size]
+    (let [bounds (make-bounds size)] 
+        (map #(map bounds %) neighbours)))
+
+(defn myfoldL [operation p_seq]
+    (loop [acc 0 s p_seq ]
+        (if (empty? s)
+            acc
+            (recur (operation acc (first s)) (rest s)))))
