113a4fa747c9ea47afd5ee59cd6c806913da5379
ufc25.bezerra <vladymirbezerra@great.ufc.br>
2015-04-07 15:56:57 -0300

    presentation code

---
+++ src/conway/presentation.clj
@@ -0,0 +1,24 @@
+(def items
+	[{:name "Laptop",     :value 2500, :discount 10, :qty 1},
+	 {:name "TV",         :value 1000, :discount 0,  :qty 2},
+	 {:name "SmartPhone", :value 1350, :discount 0,  :qty 6},
+	 {:name "Tablet",     :value 1200, :discount 0,  :qty 3},
+	 {:name "Headphone",  :value 100,  :discount 20, :qty 4},
+	 {:name "Router",     :value 65,   :discount 0,  :qty 3}])
+
+(defn calculate-discount [value discount qty] (* qty (- value (* value (/ discount 100.)))))
+
+(defn get-values [items]
+	(map :value items))
+
+(defn get-discounts [items]
+	(map :discount items))
+
+(defn get-qty [items]
+	(map :qty items))
+
+(defn calculate-all [items]
+	(map calculate-discount (get-values items) (get-discounts items) (get-qty items)))
+
+(defn finalize [items]
+	(reduce + (calculate-all items) ))
